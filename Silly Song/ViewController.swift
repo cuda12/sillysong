c5e207d4fb1a45ad120f0805799faa60fd5f94da
andy <arueesch@gmail.com>
2017-02-01 17:46:17 +0100

    finished silly song app

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
+
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
 
+    @IBAction func displayLyrics(_ sender: Any) {
+        let curName = nameField.text!
+        let bananaFanaTemplate = [
+            "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+            "Banana Fana Fo F<SHORT_NAME>",
+            "Me My Mo M<SHORT_NAME>",
+            "<FULL_NAME>"].joined(separator: "\n")
+        
+        if curName.characters.count > 0 {
+            lyricsView.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: curName)
+        }
+    }
 }
 
+
+
+func shortNameForName(name: String) -> String {
+    let list_vowels = CharacterSet(["a", "e", "i", "o", "u"])
+    var name_lower = name.lowercased()
+    var short_name = ""
+    
+    var flagVowel = false
+    for character in name_lower.unicodeScalars {
+        if (list_vowels.contains(character) || flagVowel) {
+            short_name.append(String(character))
+            flagVowel = true
+        }
+    }
+    return short_name
+}
+
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    let shortName = shortNameForName(name: fullName)
+    var lyrics = lyricsTemplate
+    lyrics = lyrics.replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+    lyrics = lyrics.replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    
+    return lyrics
+}
